82786da2a5559ea0b4018777a8ebfa57a389ed97
Robson Pinheiro <robinhopj@gmail.com>
2021-01-18 07:51:56 -0300

    primeiro commit

---
+++ cadastro/src/test/java/com/github/Robinhor10/Ifood/NativecadastroresourceIT.java
@@ -0,0 +1,9 @@
+package com.github.Robinhor10.Ifood;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativecadastroresourceIT extends cadastroresourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ pedido/src/test/java/com/github/Robinhor10/Ifood/NativepedidoresourceIT.java
@@ -0,0 +1,9 @@
+package com.github.Robinhor10.Ifood;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativepedidoresourceIT extends pedidoresourceTest {
+
+    // Execute the same tests but in native mode.
+}
