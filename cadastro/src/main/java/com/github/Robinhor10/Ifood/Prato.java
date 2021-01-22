cef6fba2a6a19cd0445a6929434ac96a2eec67e6
Robson Pinheiro <robinhopj@gmail.com>
2021-01-22 17:22:22 -0300

    feat() implementando o docker compose, declarando o banco e as classes prato,restaurante e localizacao

---
+++ cadastro/src/main/java/com/github/Robinhor10/Ifood/Prato.java
@@ -0,0 +1,28 @@
+package com.github.Robinhor10.Ifood;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+@Table(name = "prato")
+public class Prato extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    public Long id;
+
+    public String nome;
+    
+    public String descricacao;
+
+    @ManyToOne
+    public Restaurante restaurante;
+
+    public BigDecimal preco;
+}
---
+++ cadastro/src/main/java/com/github/Robinhor10/Ifood/RestauranteResource.java
@@ -0,0 +1,22 @@
+package com.github.Robinhor10.Ifood;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Path("/restaurantes")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RestauranteResource {
+
+    @GET
+    public List<PanacheEntityBase> hello() {
+        return Restaurante.listAll();
+    }
+}
